24f368eeb5cebe24e940fddae79c7b3f196b27bd
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-05-24 13:44:25 +0700

    Add project files.

---
+++ MyApi/Controllers/ExecuteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExecuteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ExecuteFile()
+        {
+            try
+            {
+                //https://localhost:7131/api/Execute
+                // Provide the path to your executable file
+                string exePath = @"D:\WTH This Code\test vscommu2\MyApi\MyApi\Content\testBasic.exe";
+                Process.Start(exePath);
+                return Ok("Executable started successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+    }
+}
